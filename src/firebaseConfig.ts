d57a6ec86b6deef70c2b8fe46a71eb6b6636a76c
uuronn <uuronprogram@gmail.com>
2022-05-20 20:29:26 +0900

    Add: firestore get

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,13 +1,16 @@
 // firebase setup
-
 import { initializeApp } from "firebase/app";
+// FireStoreへの接続情報の取得
+import { getFirestore } from "firebase/firestore";
 
 export const app = initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID
 });
+
+export const db = getFirestore(app);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from "next";
+import { db } from "../firebaseConfig";
+import { getDoc, doc } from "firebase/firestore";
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  const docRef = doc(db, "cities", "test");
+  const docSnap = await getDoc(docRef);
+
+  const json = JSON.parse(JSON.stringify(docSnap));
+
+  return {
+    props: {
+      posts: json
+    }
+  };
+};
+
+const Home: NextPage = ({ posts }) => {
+  console.log(posts);
+  console.log(posts._document.data.value.mapValue.fields.title.stringValue);
   return (
     <div>
       <h1>index</h1>
+      <h1>{posts._document.data.value.mapValue.fields.title.stringValue}</h1>
     </div>
   );
 };
