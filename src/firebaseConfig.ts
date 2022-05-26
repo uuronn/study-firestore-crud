43949a21bdd0a418142344eab11e48c15f7b8ed7
uuronn <uuronprogram@gmail.com>
2022-05-26 18:31:59 +0900

    Update: firebase

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,19 +1,19 @@
-// firebase setup
 import { initializeApp } from "firebase/app";
-// FireStoreへの接続情報の取得
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
 import { getStorage } from "firebase/storage";
 
-export const app = initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_APP_ID
-});
+};
 
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase);
+export const db = getFirestore(firebase);
+export const storage = getStorage(firebase);
---
+++ src/pages/auth.tsx
@@ -0,0 +1,25 @@
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../firebaseConfig";
+
+// // createUserWithEmailAndPassword(auth, email, password)
+// //   .then((userCredential) => {
+// //     const user = userCredential.user;
+// //   })
+// //   .catch((error) => {
+// //     const errorCode = error.code;
+// //     const errorMessage = error.message;
+// //   });
+
+// const signUp = async () => {
+//   try {
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     console.log(user);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
