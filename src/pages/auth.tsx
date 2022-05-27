b9044586c981365e59a552b99fd951bd3b2da9fd
uuronn <uuronprogram@gmail.com>
2022-05-27 15:24:49 +0900

    user auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,25 +1,55 @@
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// import { auth } from "../firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { NextPage } from "next";
+import { auth } from "../firebaseConfig";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
-// // createUserWithEmailAndPassword(auth, email, password)
-// //   .then((userCredential) => {
-// //     const user = userCredential.user;
-// //   })
-// //   .catch((error) => {
-// //     const errorCode = error.code;
-// //     const errorMessage = error.message;
-// //   });
+const Auth: NextPage = () => {
+  const [uid, setUid] = useState<string>("");
+  const [name, setName] = useState<string | null>("");
+  const provider = new GoogleAuthProvider();
 
-// const signUp = async () => {
-//   try {
-//     const userCredential = await createUserWithEmailAndPassword(
-//       auth,
-//       email,
-//       password
-//     );
-//     const user = userCredential.user;
-//     console.log(user);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+  const signIn = async () => {
+    try {
+      const res = await signInWithPopup(auth, provider);
+
+      if (!res) throw new Error("res取れてないよ");
+
+      setUid(res.user.uid);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setName(user.displayName);
+      console.log(user);
+      console.log(user.displayName);
+      const uid = user.uid;
+      console.log(uid);
+    } else {
+      console.log("else");
+    }
+  });
+
+  // const email = "test";
+  // const password = "testPass";
+  // createUserWithEmailAndPassword(auth, email, password)
+  //   .then((userCredential) => {
+  //     const user = userCredential.user;
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+
+  return (
+    <>
+      <h1>title</h1>
+      <button onClick={signIn}>this is test</button>
+      <p>ユーザー名: {uid}</p>
+      <p>ログイン中のユーザー: {name}</p>
+    </>
+  );
+};
+export default Auth;
